763abd434b12ad5a3e2494fc25471e8a15df9a62
RemoThim <remo.thim@stud.kftg.ch>
2022-02-07 07:31:47 +0100

    Inventar&Verbauch

--- Freezing_Inventory/www/js/Benutzung.js
+++ Freezing_Inventory/www/js/Benutzung.js
@@ -1,52 +1,62 @@
-var marker;
-var onSuccess = function (position) {
-    $(window).height()
-    if (marker != null) {
-        mymap.removeLayer(marker);
-    }
-    mymap.setView([position.coords.latitude, position.coords.longitude], 17);
-    marker = L.marker([position.coords.latitude, position.coords.longitude]);
-    mymap.addLayer(marker);
-};
-function onError(error) {
-    alert('code: ' + error.code + '\n' +
-        'message: ' + error.message + '\n');
+var db = window.openDatabase("Inventory", "1.0", "Inventory", 200000);
+db.transaction(Benutzung, errorCB);
+db.transaction(queryDB, errorCB);
+var row;
+
+function Benutzung(tx) {
+    tx.executeSql("SELECT ArtName,ArtId,strftime('%d.%m.%Y',ArtAblaufdatum) as ptime FROM TArtikel", [], fillBenutzung, errorCB);
 }
-var setLngLat = function(LngLat){
-    var FahrName = firebase.database().ref("Fahrzeuge/" + id_aenderne + "/FahrzeugName");
-    var id = firebase.database().ref("Fahrzeuge/" + id_aenderne + "/id");
+function fillBenutzung(tx, results) {
+    var VerHtml = '<label><b>Artikel:</b></label><br><select id="select" class="form-select">'
+    var len = results.rows.length;
+    for (var i = 0; i < len; i++) {
+        VerHtml += "<option value=" + results.rows.item(i).ArtId + ">" + results.rows.item(i).ArtName + " vom " + results.rows.item(i).ptime + "</option>";
+    }
+    VerHtml += '</select><br><label><b>Anzahl:</b></label><br><input type="number" id="Anzahl" class="form-control w-25" min="1"><button id="VerbButton" class="btn btn-light" onclick="Verbauch()">Verbrauchen</button>'
+    document.getElementById("verbrauch").innerHTML = VerHtml;
+}
+function errorCB(err) {
+    alert("Error processing SQL: " + err.code);
+}
+function queryDB(tx) {
+    tx.executeSql("SELECT ArtName,ArtAnz,ArtId,strftime('%d.%m.%Y',ArtAblaufdatum) as ptime FROM TArtikel ", [], querySuccess, errorCB);
+}
+function querySuccess(tx, results) {
+    var tblText = '<table class="table table-striped table-bordered" id="info"><thead><tr><th>Name</th> <th>Anzahl</th> <th>Ablaufdatum</th></tr></thead><tbody>';
+    var len = results.rows.length;
+    if (len >= 1) {
+        for (var i = 0; i < len; i++) {
+            if (results.rows.item(i).ArtAnz < 1) {
+                deleteRow(results.rows.item(i).ArtId);
+            } else {
+                tblText += '<tr><td>' + results.rows.item(i).ArtName + '</td><td>' + results.rows.item(i).ArtAnz + '</td><td>' + results.rows.item(i).ptime + '</td></tr>';
+            }
+        }
+        tblText += "</tbody></table>";
+        document.getElementById("table").innerHTML = tblText;
+    }
+}
+function Verbauch() {
+    var db = window.openDatabase("Inventory", "1.0", "Inventory", 200000);
+    var wert = document.getElementById("Anzahl");
+    if (wert.value < 1) {
+        alert("Die Anzahl muss grössergleich 1 sein!")
+    } else {
+        db.transaction(VerbrauchSQL, errorCB);
+    }
+}
+function VerbrauchSQL(tx) {
+    var auswahl = document.getElementById("select");
+    var wert = document.getElementById("Anzahl")
+    tx.executeSql("UPDATE TArtikel set ArtAnz = ArtAnz - " + wert.value + " where ArtId = " + auswahl.value, [], queryDB, errorCB);
+}
+function deleteRow(ID) {
+    row = ID;
+    var db = window.openDatabase("Inventory", "1.0", "Inventory", 200000);
     
-    FahrName.on("value", function(snapshot) {
-         FahrName = snapshot.val();
-
-    })
-    id.on("value", function(snapshot) {
-        id = snapshot.val();
-
-   })
-    firebase.database().ref("Fahrzeuge/" + id_aenderne).set({
-        FahrzeugName : FahrName,
-        id: id,
-        latitude: LngLat.coords.latitude,
-        longitude: LngLat.coords.longitude
-    });
-} 
-var id_aenderne;
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
-showData();
-function showData() {
-    var ref = firebase.database().ref();
-    ref.on("value", function (response) {
-        var template = $('#template').html();
-        var html = Mustache.render(template, response.val());
-        $('#tabelle tbody').html(html);
-        $(".parken").click(function (e) { 
-            e.preventDefault();
-             id_aenderne = $(this).attr('data-id') - 1;
-             navigator.geolocation.getCurrentPosition(setLngLat, onError);
-
-        });
-    }, function (error) {
-        console.log("Error: " + error.message);
-    });
+    db.transaction(lol, errorCB);
+}
+function lol(tx) {
+    tx.executeSql('DELETE FROM TArtikel WHERE ArtId = ' + row, [], queryDB, errorCB)
+    db.transaction(Benutzung, errorCB); 
 }
--- Freezing_Inventory/www/js/Inventar.js
+++ Freezing_Inventory/www/js/Inventar.js
@@ -1,8 +1,8 @@
 var db = window.openDatabase("Inventory", "1.0", "Inventory", 200000);
 db.transaction(populateDB);
-db.transaction(insertDB, errorCB, successCB);
-var select = 'SELECT ArtName,ArtAnz,ArtAblaufdatum FROM TArtikel'
-db.transaction(dropdown, errorCB)
+var select = "SELECT ArtName,ArtAnz,strftime('%d.%m.%Y',ArtAblaufdatum) as ptime FROM TArtikel";
+db.transaction( insertDB, errorCB ,successCB);
+db.transaction(dropdown, errorCB);
 
 
 function populateDB(tx) {
@@ -13,11 +13,11 @@ function queryDB(tx) {
     tx.executeSql(select, [], querySuccess, errorCB);
 }
 function querySuccess(tx, results) {
+    console.log(results);
     var tblText = '<table class="table table-striped table-bordered"><thead><tr><th>Name</th> <th>Anzahl</th> <th>Ablaufdatum</th></tr></thead><tbody>';
     var len = results.rows.length;
     for (var i = 0; i < len; i++) {
-        //var tmpArgs = results.rows.item(i).ArtName + ",'" + results.rows.item(i).ProAnz + "','" + results.rows.item(i).ProAblaufdatum + "'";
-        tblText += '<tr><td>' + results.rows.item(i).ArtName + '</td><td>' + results.rows.item(i).ArtAnz + '</td><td>' + results.rows.item(i).ArtAblaufdatum + '</td></tr>';
+        tblText += '<tr><td>' + results.rows.item(i).ArtName + '</td><td>' + results.rows.item(i).ArtAnz + '</td><td>' + results.rows.item(i).ptime + '</td></tr>';
     }
     tblText += "</tbody></table>";
     document.getElementById("table").innerHTML = tblText;
@@ -34,14 +34,14 @@ function successCB() {
 }
 function insertDB(tx) {
     //tx.executeSql('INSERT INTO TKategorie (KatId,KatName) VALUES (1,"Fleisch"),(2,"Obst")');
-    // tx.executeSql('INSERT INTO TArtikel (KatID,ArtName,ArtAnz,ArtAblaufdatum) VALUES (1,"Iglo Fischstäbchen",2,"2022-03-15"),(2,"Apfel",2,"2022-10-16")');
+   tx.executeSql('INSERT INTO TArtikel (KatID,ArtName,ArtAnz,ArtAblaufdatum) VALUES (1,"Iglo Fischstäbchen",2,"2022-03-15"),(2,"Apfel",2,"2022-10-16")');
 }
 
 function dropdown(tx) {
     tx.executeSql('Select * from TKategorie', [], filldropdown, errorCB);
 }
 function filldropdown(tx, results) {
-    var dropHtml = '<select onchange="KatWahl(this)"><option value=0>Alle</option>'
+    var dropHtml = '<label><b>Kategorie:</b></label><br><select onchange="KatWahl(this)" class="form-select w-50"><option value=0>Alle</option>'
     var len = results.rows.length;
     for (var i = 0; i < len; i++) {
         dropHtml += "<option value=" + results.rows.item(i).KatId + ">" + results.rows.item(i).KatName + "</option>";
@@ -53,12 +53,11 @@ function KatWahl(selectObject, tx) {
     var Auswahl = selectObject.value;
     console.log(Auswahl);
     if (Auswahl == 0) {
-        select = 'SELECT ArtName,ArtAnz,ArtAblaufdatum FROM TArtikel';
+        select = "SELECT ArtName,ArtAnz,strftime('%d.%m.%Y',ArtAblaufdatum) as ptime FROM TArtikel";
     } else {
-        select = "SELECT ArtName,ArtAnz,ArtAblaufdatum FROM TArtikel where KatId = " + Auswahl
+        select = "SELECT ArtName,ArtAnz,strftime('%d.%m.%Y',ArtAblaufdatum) as ptime FROM TArtikel where KatId = " + Auswahl
     }
-    console.log(select);
-    db.transaction(execute, errorCB)
+    db.transaction(KatAnzeige, errorCB)
 }
 function KatAnzeige(tx) {
     tx.executeSql(select, [], querySuccess, errorCB);
